e30632df28230ebb0037efc2ad20cafb207bee6e
Kenta Tanaka <k-tanaka@sig-c.co.jp>
2017-06-20 18:01:34 +0900

    テキストエリアが文字数に応じて大きくなる部分の追加
    削除、編集ボタンの丸み削除

--- public/assets/js/edit.js
+++ public/assets/js/edit.js
@@ -6,6 +6,7 @@ var edit = function(){
       var text = $('p#'+msg_id).text();
       $('p#'+msg_id).html('<textarea id="edit"  rows="6" maxlength="80">'+text+'</textarea>');
       // $('textarea').autosize();
+      textarea();
       $('p#'+msg_id+' > textarea').focus().blur(function(){
         var inputValue = $(this).val();
         if(inputValue==''){
@@ -36,3 +37,13 @@ var edit = function(){
   });
 }
 
+var textarea  = function(){
+  $('textarea').focus(function(e) {
+    //文字数から高さ取得
+    var height=this.scrollHeight + 'px';
+    $(this).css("height", height);
+    })
+    .blur(function(e) {
+    $(this).css("height", "auto");
+  });
+}
